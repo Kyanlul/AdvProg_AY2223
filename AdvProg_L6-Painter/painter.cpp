9e3421d20a2bf0a70530ea3b437e5e247e6fd6cd
Kyanlul <quydanglc123@gmail.com>
2023-05-04 22:22:55 +0700

    Painter2

--- AdvProg_L6-Painter/painter.cpp
+++ AdvProg_L6-Painter/painter.cpp
@@ -9,7 +9,7 @@
 void Painter::setColor(SDL_Color color) 
 { 
     // TODO: set the color value for the Painter and set Render Draw Color
-     this->color = ;
+     this->color = color;
     SDL_SetRenderDrawColor( this->renderer, color.r, color.g, color.b, color.a );
 }
 
